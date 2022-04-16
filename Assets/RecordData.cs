ed0b6ffd0302bc2554a0701977dbb62620b66643
bruce23926 <66370857+bruce23926@users.noreply.github.com>
2022-04-16 10:51:54 +0800

    upload unity file

---
+++ Assets/RecordData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordData : MonoBehaviour {
+
+	// Use this for initialization
+	public FileWriter FW;
+	public string Data;
+	public bool Flag;
+	int count = 0;
+	public int CountClock = 30;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (count <= 0) {
+			Data = gameObject.transform.position.x.ToString();
+			Data += ","+gameObject.transform.position.y.ToString();
+			Data += ","+gameObject.transform.position.z.ToString();
+			Data += ","+Flag.ToString();
+			FW.WriteFile(Data);//Write string Data as a line of data
+			count = CountClock;
+		} else {
+			count -= 1;
+		}
+	}
+}
